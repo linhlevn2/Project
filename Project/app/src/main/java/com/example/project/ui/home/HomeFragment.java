ea6844904ae67557b092cc7db941f10ac06804a2
linhlevn2 <linhlevn2@gmail.com>
2020-11-09 03:01:14 -0500

    fetch and update

--- Project/app/src/main/java/com/example/project/ui/home/HomeFragment.java
+++ Project/app/src/main/java/com/example/project/ui/home/HomeFragment.java
@@ -14,11 +14,13 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.project.R;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
@@ -44,20 +46,21 @@ public class HomeFragment extends Fragment {
         postBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                saveInfo(v);
+                save_Fetch_Update_Info(v);
             }
         });
         return root;
     }
+        Map<String, Object> review = new HashMap<>();
         private DocumentReference mDocRef = FirebaseFirestore.getInstance().document("users/user");
-        public void saveInfo(View view){
+        public void save_Fetch_Update_Info(View view){
             EditText reviewView = root.findViewById(R.id.textReview);
-            String reviewText = reviewView.getText().toString();
+            final String reviewText = reviewView.getText().toString();
             //TextView userView = root.findViewById(R.id.emailText);
             //String userEmail = userView.getText().toString();
 
             if (reviewText.isEmpty()) {return;}
-            Map<String, Object> review = new HashMap<>();
+
             review.put("user", reviewText);
 
             mDocRef.set(review).addOnSuccessListener(new OnSuccessListener<Void>() {
@@ -71,6 +74,18 @@ public class HomeFragment extends Fragment {
                     Log.d("userReview", "Document was not saved!", e);
                 }
             });
+
+            final TextView reviewsText = root.findViewById(R.id.reviews);
+            mDocRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                @Override
+                public void onSuccess(DocumentSnapshot documentSnapshot) {
+                    if (documentSnapshot.exists()){
+                        String reviewGetText = documentSnapshot.getString(reviewText);
+                        reviewsText.setText(reviewText+"\n");
+                    }
+                }
+            });
+
 
         }
 
