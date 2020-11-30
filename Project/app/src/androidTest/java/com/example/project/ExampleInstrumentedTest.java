6677ddfd5db111db387560f8384818f40dc4704a
linhlevn2 <linhlevn2@gmail.com>
2020-11-30 02:35:26 -0500

    write unit test

--- Project/app/src/androidTest/java/com/example/project/ExampleInstrumentedTest.java
+++ Project/app/src/androidTest/java/com/example/project/ExampleInstrumentedTest.java
@@ -2,12 +2,24 @@ package com.example.project;
 
 import android.content.Context;
 
+import androidx.test.espresso.Espresso;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
 
+//import com.google.android.apps.common.testing.ui.espresso.Espresso;
+
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 /**
@@ -22,5 +34,28 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.project", appContext.getPackageName());
+    }
+
+    @Rule
+    public ActivityTestRule mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
+    @Test
+    public void activityLaunch1() {
+        Espresso.onView(withId(R.id.button)).perform(click());
+        Espresso.onView(withId(R.id.textView6)).check(matches(isDisplayed()));
+        Espresso.onView(withId(R.id.button3)).perform(click());
+        Espresso.onView(withId(R.id.textView6)).check(matches(isDisplayed()));
+    }
+
+    public void activitiLaunch2() {
+
+    }
+
+    @Test
+    public void textInputOutput() {
+        Espresso.onView(withId(R.id.reviewTxt)).perform(typeText("This is a test review."));
+        Espresso.onView(withId(R.id.button)).perform(scrollTo()).perform(click());
+        Espresso.onView(withId(R.id.textView6)).check(matches(withText("Hotel#: 01\nReview: This is a test review.\n\n")));
     }
 }
